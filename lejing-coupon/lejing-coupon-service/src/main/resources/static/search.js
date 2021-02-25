8ebd4570153074f38dc852808741f67e7999cc70
Weasley <1432689025@qq.com>
2021-02-26 01:54:36 +0800

    add

---
+++ lejing-coupon/lejing-coupon-service/src/main/resources/static/search.js
@@ -0,0 +1,533 @@
+let api = [];
+api.push({
+    alias: 'CouponController',
+    order: '1',
+    link: '优惠券信息controller',
+    desc: '优惠券信息Controller',
+    list: []
+})
+api[0].list.push({
+    order: '1',
+    desc: '查询优惠券信息列表',
+});
+api[0].list.push({
+    order: '2',
+    desc: '获取优惠券信息详情',
+});
+api[0].list.push({
+    order: '3',
+    desc: '新增优惠券信息',
+});
+api[0].list.push({
+    order: '4',
+    desc: '修改优惠券信息',
+});
+api[0].list.push({
+    order: '5',
+    desc: '批量删除优惠券信息',
+});
+api.push({
+    alias: 'CouponHistoryController',
+    order: '2',
+    link: '优惠券领取历史记录controller',
+    desc: '优惠券领取历史记录Controller',
+    list: []
+})
+api[1].list.push({
+    order: '1',
+    desc: '查询优惠券领取历史记录列表',
+});
+api[1].list.push({
+    order: '2',
+    desc: '获取优惠券领取历史记录详情',
+});
+api[1].list.push({
+    order: '3',
+    desc: '新增优惠券领取历史记录',
+});
+api[1].list.push({
+    order: '4',
+    desc: '修改优惠券领取历史记录',
+});
+api[1].list.push({
+    order: '5',
+    desc: '批量删除优惠券领取历史记录',
+});
+api.push({
+    alias: 'CouponSpuCategoryRelationController',
+    order: '3',
+    link: '优惠券分类关联controller',
+    desc: '优惠券分类关联Controller',
+    list: []
+})
+api[2].list.push({
+    order: '1',
+    desc: '查询优惠券分类关联列表',
+});
+api[2].list.push({
+    order: '2',
+    desc: '获取优惠券分类关联详情',
+});
+api[2].list.push({
+    order: '3',
+    desc: '新增优惠券分类关联',
+});
+api[2].list.push({
+    order: '4',
+    desc: '修改优惠券分类关联',
+});
+api[2].list.push({
+    order: '5',
+    desc: '批量删除优惠券分类关联',
+});
+api.push({
+    alias: 'CouponSpuRelationController',
+    order: '4',
+    link: '优惠券与产品关联controller',
+    desc: '优惠券与产品关联Controller',
+    list: []
+})
+api[3].list.push({
+    order: '1',
+    desc: '查询优惠券与产品关联列表',
+});
+api[3].list.push({
+    order: '2',
+    desc: '获取优惠券与产品关联详情',
+});
+api[3].list.push({
+    order: '3',
+    desc: '新增优惠券与产品关联',
+});
+api[3].list.push({
+    order: '4',
+    desc: '修改优惠券与产品关联',
+});
+api[3].list.push({
+    order: '5',
+    desc: '批量删除优惠券与产品关联',
+});
+api.push({
+    alias: 'HomeAdvController',
+    order: '5',
+    link: '首页轮播广告controller',
+    desc: '首页轮播广告Controller',
+    list: []
+})
+api[4].list.push({
+    order: '1',
+    desc: '查询首页轮播广告列表',
+});
+api[4].list.push({
+    order: '2',
+    desc: '获取首页轮播广告详情',
+});
+api[4].list.push({
+    order: '3',
+    desc: '新增首页轮播广告',
+});
+api[4].list.push({
+    order: '4',
+    desc: '修改首页轮播广告',
+});
+api[4].list.push({
+    order: '5',
+    desc: '批量删除首页轮播广告',
+});
+api.push({
+    alias: 'HomeSubjectController',
+    order: '6',
+    link: '首页专题表【jd首页下面很多专题，每个专题链接新的页面，展示专题商品信息】controller',
+    desc: '首页专题表【jd首页下面很多专题，每个专题链接新的页面，展示专题商品信息】Controller',
+    list: []
+})
+api[5].list.push({
+    order: '1',
+    desc: '查询首页专题表【jd首页下面很多专题，每个专题链接新的页面，展示专题商品信息】列表',
+});
+api[5].list.push({
+    order: '2',
+    desc: '获取首页专题表【jd首页下面很多专题，每个专题链接新的页面，展示专题商品信息】详情',
+});
+api[5].list.push({
+    order: '3',
+    desc: '新增首页专题表【jd首页下面很多专题，每个专题链接新的页面，展示专题商品信息】',
+});
+api[5].list.push({
+    order: '4',
+    desc: '修改首页专题表【jd首页下面很多专题，每个专题链接新的页面，展示专题商品信息】',
+});
+api[5].list.push({
+    order: '5',
+    desc: '批量删除首页专题表【jd首页下面很多专题，每个专题链接新的页面，展示专题商品信息】',
+});
+api.push({
+    alias: 'HomeSubjectSpuController',
+    order: '7',
+    link: '专题商品controller',
+    desc: '专题商品Controller',
+    list: []
+})
+api[6].list.push({
+    order: '1',
+    desc: '查询专题商品列表',
+});
+api[6].list.push({
+    order: '2',
+    desc: '获取专题商品详情',
+});
+api[6].list.push({
+    order: '3',
+    desc: '新增专题商品',
+});
+api[6].list.push({
+    order: '4',
+    desc: '修改专题商品',
+});
+api[6].list.push({
+    order: '5',
+    desc: '批量删除专题商品',
+});
+api.push({
+    alias: 'MemberPriceController',
+    order: '8',
+    link: '商品会员价格controller',
+    desc: '商品会员价格Controller',
+    list: []
+})
+api[7].list.push({
+    order: '1',
+    desc: '查询商品会员价格列表',
+});
+api[7].list.push({
+    order: '2',
+    desc: '获取商品会员价格详情',
+});
+api[7].list.push({
+    order: '3',
+    desc: '新增商品会员价格',
+});
+api[7].list.push({
+    order: '4',
+    desc: '修改商品会员价格',
+});
+api[7].list.push({
+    order: '5',
+    desc: '批量删除商品会员价格',
+});
+api.push({
+    alias: 'SeckillPromotionController',
+    order: '9',
+    link: '秒杀活动controller',
+    desc: '秒杀活动Controller',
+    list: []
+})
+api[8].list.push({
+    order: '1',
+    desc: '查询秒杀活动列表',
+});
+api[8].list.push({
+    order: '2',
+    desc: '获取秒杀活动详情',
+});
+api[8].list.push({
+    order: '3',
+    desc: '新增秒杀活动',
+});
+api[8].list.push({
+    order: '4',
+    desc: '修改秒杀活动',
+});
+api[8].list.push({
+    order: '5',
+    desc: '批量删除秒杀活动',
+});
+api.push({
+    alias: 'SeckillSessionController',
+    order: '10',
+    link: '秒杀活动场次controller',
+    desc: '秒杀活动场次Controller',
+    list: []
+})
+api[9].list.push({
+    order: '1',
+    desc: '查询秒杀活动场次列表',
+});
+api[9].list.push({
+    order: '2',
+    desc: '获取秒杀活动场次详情',
+});
+api[9].list.push({
+    order: '3',
+    desc: '新增秒杀活动场次',
+});
+api[9].list.push({
+    order: '4',
+    desc: '修改秒杀活动场次',
+});
+api[9].list.push({
+    order: '5',
+    desc: '批量删除秒杀活动场次',
+});
+api.push({
+    alias: 'SeckillSkuNoticeController',
+    order: '11',
+    link: '秒杀商品通知订阅controller',
+    desc: '秒杀商品通知订阅Controller',
+    list: []
+})
+api[10].list.push({
+    order: '1',
+    desc: '查询秒杀商品通知订阅列表',
+});
+api[10].list.push({
+    order: '2',
+    desc: '获取秒杀商品通知订阅详情',
+});
+api[10].list.push({
+    order: '3',
+    desc: '新增秒杀商品通知订阅',
+});
+api[10].list.push({
+    order: '4',
+    desc: '修改秒杀商品通知订阅',
+});
+api[10].list.push({
+    order: '5',
+    desc: '批量删除秒杀商品通知订阅',
+});
+api.push({
+    alias: 'SeckillSkuRelationController',
+    order: '12',
+    link: '秒杀活动商品关联controller',
+    desc: '秒杀活动商品关联Controller',
+    list: []
+})
+api[11].list.push({
+    order: '1',
+    desc: '查询秒杀活动商品关联列表',
+});
+api[11].list.push({
+    order: '2',
+    desc: '获取秒杀活动商品关联详情',
+});
+api[11].list.push({
+    order: '3',
+    desc: '新增秒杀活动商品关联',
+});
+api[11].list.push({
+    order: '4',
+    desc: '修改秒杀活动商品关联',
+});
+api[11].list.push({
+    order: '5',
+    desc: '批量删除秒杀活动商品关联',
+});
+api.push({
+    alias: 'SkuFullReductionController',
+    order: '13',
+    link: '商品满减信息controller',
+    desc: '商品满减信息Controller',
+    list: []
+})
+api[12].list.push({
+    order: '1',
+    desc: '查询商品满减信息列表',
+});
+api[12].list.push({
+    order: '2',
+    desc: '获取商品满减信息详情',
+});
+api[12].list.push({
+    order: '3',
+    desc: '新增商品满减信息',
+});
+api[12].list.push({
+    order: '4',
+    desc: '修改商品满减信息',
+});
+api[12].list.push({
+    order: '5',
+    desc: '批量删除商品满减信息',
+});
+api[12].list.push({
+    order: '6',
+    desc: '保存满减、优惠信息',
+});
+api.push({
+    alias: 'SkuLadderController',
+    order: '14',
+    link: '商品阶梯价格controller',
+    desc: '商品阶梯价格Controller',
+    list: []
+})
+api[13].list.push({
+    order: '1',
+    desc: '查询商品阶梯价格列表',
+});
+api[13].list.push({
+    order: '2',
+    desc: '获取商品阶梯价格详情',
+});
+api[13].list.push({
+    order: '3',
+    desc: '新增商品阶梯价格',
+});
+api[13].list.push({
+    order: '4',
+    desc: '修改商品阶梯价格',
+});
+api[13].list.push({
+    order: '5',
+    desc: '批量删除商品阶梯价格',
+});
+api.push({
+    alias: 'SpuBoundsController',
+    order: '15',
+    link: '商品spu积分设置controller',
+    desc: '商品spu积分设置Controller',
+    list: []
+})
+api[14].list.push({
+    order: '1',
+    desc: '查询商品spu积分设置列表',
+});
+api[14].list.push({
+    order: '2',
+    desc: '获取商品spu积分设置详情',
+});
+api[14].list.push({
+    order: '3',
+    desc: '新增商品spu积分设置',
+});
+api[14].list.push({
+    order: '4',
+    desc: '修改商品spu积分设置',
+});
+api[14].list.push({
+    order: '5',
+    desc: '批量删除商品spu积分设置',
+});
+api.push({
+    alias: 'UndoLogController',
+    order: '16',
+    link: '撤销日志表controller',
+    desc: '撤销日志表Controller',
+    list: []
+})
+api[15].list.push({
+    order: '1',
+    desc: '查询撤销日志表列表',
+});
+api[15].list.push({
+    order: '2',
+    desc: '获取撤销日志表详情',
+});
+api[15].list.push({
+    order: '3',
+    desc: '新增撤销日志表',
+});
+api[15].list.push({
+    order: '4',
+    desc: '修改撤销日志表',
+});
+api[15].list.push({
+    order: '5',
+    desc: '批量删除撤销日志表',
+});
+api.push({
+    alias: 'dict',
+    order: '17',
+    link: 'dict_list',
+    desc: '数据字典',
+    list: []
+})
+document.onkeydown = keyDownSearch;
+function keyDownSearch(e) {
+    const theEvent = e;
+    const code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+    if (code == 13) {
+        const search = document.getElementById('search');
+        const searchValue = search.value;
+        let searchArr = [];
+        for (let i = 0; i < api.length; i++) {
+            let apiData = api[i];
+            const desc = apiData.desc;
+            if (desc.indexOf(searchValue) > -1) {
+                searchArr.push({
+                    order: apiData.order,
+                    desc: apiData.desc,
+                    link: apiData.link,
+                    list: apiData.list
+                });
+            } else {
+                let methodList = apiData.list || [];
+                let methodListTemp = [];
+                for (let j = 0; j < methodList.length; j++) {
+                    const methodData = methodList[j];
+                    const methodDesc = methodData.desc;
+                    if (methodDesc.indexOf(searchValue) > -1) {
+                        methodListTemp.push(methodData);
+                        break;
+                    }
+                }
+                if (methodListTemp.length > 0) {
+                    const data = {
+                        order: apiData.order,
+                        desc: apiData.desc,
+                        link: apiData.link,
+                        list: methodListTemp
+                    };
+                    searchArr.push(data);
+                }
+            }
+        }
+        let html;
+        if (searchValue == '') {
+            const liClass = "";
+            const display = "display: none";
+            html = buildAccordion(api,liClass,display);
+            document.getElementById('accordion').innerHTML = html;
+        } else {
+            const liClass = "open";
+            const display = "display: block";
+            html = buildAccordion(searchArr,liClass,display);
+            document.getElementById('accordion').innerHTML = html;
+        }
+        const Accordion = function (el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+            const links = this.el.find('.dd');
+            links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+        };
+        Accordion.prototype.dropdown = function (e) {
+            const $el = e.data.el;
+            $this = $(this), $next = $this.next();
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp("20").parent().removeClass('open');
+            }
+        };
+        new Accordion($('#accordion'), false);
+    }
+}
+
+function buildAccordion(apiData, liClass, display) {
+    let html = "";
+    let doc;
+    if (apiData.length > 0) {
+        for (let j = 0; j < apiData.length; j++) {
+            html += '<li class="'+liClass+'">';
+            html += '<a class="dd" href="#_' + apiData[j].link + '">' + apiData[j].order + '.&nbsp;' + apiData[j].desc + '</a>';
+            html += '<ul class="sectlevel2" style="'+display+'">';
+            doc = apiData[j].list;
+            for (let m = 0; m < doc.length; m++) {
+                html += '<li><a href="#_' + apiData[j].order + '_' + doc[m].order + '_' + doc[m].desc + '">' + apiData[j].order + '.' + doc[m].order + '.&nbsp;' + doc[m].desc + '</a> </li>';
+            }
+            html += '</ul>';
+            html += '</li>';
+        }
+    }
+    return html;
+}
---
+++ lejing-ware/lejing-ware-service/src/main/resources/static/search.js
@@ -0,0 +1,286 @@
+let api = [];
+api.push({
+    alias: 'PurchaseController',
+    order: '1',
+    link: '采购信息controller',
+    desc: '采购信息Controller',
+    list: []
+})
+api[0].list.push({
+    order: '1',
+    desc: '查询采购信息列表',
+});
+api[0].list.push({
+    order: '2',
+    desc: '获取采购信息详情',
+});
+api[0].list.push({
+    order: '3',
+    desc: '新增采购信息',
+});
+api[0].list.push({
+    order: '4',
+    desc: '修改采购信息',
+});
+api[0].list.push({
+    order: '5',
+    desc: '批量删除采购信息',
+});
+api.push({
+    alias: 'PurchaseDetailController',
+    order: '2',
+    link: '仓储采购表controller',
+    desc: '仓储采购表Controller',
+    list: []
+})
+api[1].list.push({
+    order: '1',
+    desc: '查询仓储采购表列表',
+});
+api[1].list.push({
+    order: '2',
+    desc: '获取仓储采购表详情',
+});
+api[1].list.push({
+    order: '3',
+    desc: '新增仓储采购表',
+});
+api[1].list.push({
+    order: '4',
+    desc: '修改仓储采购表',
+});
+api[1].list.push({
+    order: '5',
+    desc: '批量删除仓储采购表',
+});
+api.push({
+    alias: 'UndoLogController',
+    order: '3',
+    link: '撤销日志表controller',
+    desc: '撤销日志表Controller',
+    list: []
+})
+api[2].list.push({
+    order: '1',
+    desc: '查询撤销日志表列表',
+});
+api[2].list.push({
+    order: '2',
+    desc: '获取撤销日志表详情',
+});
+api[2].list.push({
+    order: '3',
+    desc: '新增撤销日志表',
+});
+api[2].list.push({
+    order: '4',
+    desc: '修改撤销日志表',
+});
+api[2].list.push({
+    order: '5',
+    desc: '批量删除撤销日志表',
+});
+api.push({
+    alias: 'WareInfoController',
+    order: '4',
+    link: '仓库信息controller',
+    desc: '仓库信息Controller',
+    list: []
+})
+api[3].list.push({
+    order: '1',
+    desc: '查询仓库信息列表',
+});
+api[3].list.push({
+    order: '2',
+    desc: '获取仓库信息详情',
+});
+api[3].list.push({
+    order: '3',
+    desc: '新增仓库信息',
+});
+api[3].list.push({
+    order: '4',
+    desc: '修改仓库信息',
+});
+api[3].list.push({
+    order: '5',
+    desc: '批量删除仓库信息',
+});
+api.push({
+    alias: 'WareOrderTaskController',
+    order: '5',
+    link: '库存工作单controller',
+    desc: '库存工作单Controller',
+    list: []
+})
+api[4].list.push({
+    order: '1',
+    desc: '查询库存工作单列表',
+});
+api[4].list.push({
+    order: '2',
+    desc: '获取库存工作单详情',
+});
+api[4].list.push({
+    order: '3',
+    desc: '新增库存工作单',
+});
+api[4].list.push({
+    order: '4',
+    desc: '修改库存工作单',
+});
+api[4].list.push({
+    order: '5',
+    desc: '批量删除库存工作单',
+});
+api.push({
+    alias: 'WareOrderTaskDetailController',
+    order: '6',
+    link: '库存工作单controller',
+    desc: '库存工作单Controller',
+    list: []
+})
+api[5].list.push({
+    order: '1',
+    desc: '查询库存工作单列表',
+});
+api[5].list.push({
+    order: '2',
+    desc: '获取库存工作单详情',
+});
+api[5].list.push({
+    order: '3',
+    desc: '新增库存工作单',
+});
+api[5].list.push({
+    order: '4',
+    desc: '修改库存工作单',
+});
+api[5].list.push({
+    order: '5',
+    desc: '批量删除库存工作单',
+});
+api.push({
+    alias: 'WareSkuController',
+    order: '7',
+    link: '商品库存controller',
+    desc: '商品库存Controller',
+    list: []
+})
+api[6].list.push({
+    order: '1',
+    desc: '查询商品库存列表',
+});
+api[6].list.push({
+    order: '2',
+    desc: '获取商品库存详情',
+});
+api[6].list.push({
+    order: '3',
+    desc: '新增商品库存',
+});
+api[6].list.push({
+    order: '4',
+    desc: '修改商品库存',
+});
+api[6].list.push({
+    order: '5',
+    desc: '批量删除商品库存',
+});
+api.push({
+    alias: 'dict',
+    order: '8',
+    link: 'dict_list',
+    desc: '数据字典',
+    list: []
+})
+document.onkeydown = keyDownSearch;
+function keyDownSearch(e) {
+    const theEvent = e;
+    const code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+    if (code == 13) {
+        const search = document.getElementById('search');
+        const searchValue = search.value;
+        let searchArr = [];
+        for (let i = 0; i < api.length; i++) {
+            let apiData = api[i];
+            const desc = apiData.desc;
+            if (desc.indexOf(searchValue) > -1) {
+                searchArr.push({
+                    order: apiData.order,
+                    desc: apiData.desc,
+                    link: apiData.link,
+                    list: apiData.list
+                });
+            } else {
+                let methodList = apiData.list || [];
+                let methodListTemp = [];
+                for (let j = 0; j < methodList.length; j++) {
+                    const methodData = methodList[j];
+                    const methodDesc = methodData.desc;
+                    if (methodDesc.indexOf(searchValue) > -1) {
+                        methodListTemp.push(methodData);
+                        break;
+                    }
+                }
+                if (methodListTemp.length > 0) {
+                    const data = {
+                        order: apiData.order,
+                        desc: apiData.desc,
+                        link: apiData.link,
+                        list: methodListTemp
+                    };
+                    searchArr.push(data);
+                }
+            }
+        }
+        let html;
+        if (searchValue == '') {
+            const liClass = "";
+            const display = "display: none";
+            html = buildAccordion(api,liClass,display);
+            document.getElementById('accordion').innerHTML = html;
+        } else {
+            const liClass = "open";
+            const display = "display: block";
+            html = buildAccordion(searchArr,liClass,display);
+            document.getElementById('accordion').innerHTML = html;
+        }
+        const Accordion = function (el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+            const links = this.el.find('.dd');
+            links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+        };
+        Accordion.prototype.dropdown = function (e) {
+            const $el = e.data.el;
+            $this = $(this), $next = $this.next();
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp("20").parent().removeClass('open');
+            }
+        };
+        new Accordion($('#accordion'), false);
+    }
+}
+
+function buildAccordion(apiData, liClass, display) {
+    let html = "";
+    let doc;
+    if (apiData.length > 0) {
+        for (let j = 0; j < apiData.length; j++) {
+            html += '<li class="'+liClass+'">';
+            html += '<a class="dd" href="#_' + apiData[j].link + '">' + apiData[j].order + '.&nbsp;' + apiData[j].desc + '</a>';
+            html += '<ul class="sectlevel2" style="'+display+'">';
+            doc = apiData[j].list;
+            for (let m = 0; m < doc.length; m++) {
+                html += '<li><a href="#_' + apiData[j].order + '_' + doc[m].order + '_' + doc[m].desc + '">' + apiData[j].order + '.' + doc[m].order + '.&nbsp;' + doc[m].desc + '</a> </li>';
+            }
+            html += '</ul>';
+            html += '</li>';
+        }
+    }
+    return html;
+}
