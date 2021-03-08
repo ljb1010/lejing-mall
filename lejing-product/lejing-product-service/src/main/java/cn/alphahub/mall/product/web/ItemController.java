bae5bf06ebb005452df9b50500c3a7c39a39b1e2
Weasley <1432689025@qq.com>
2021-03-09 01:32:34 +0800

    add template.

---
+++ lejing-product/lejing-product-service/src/main/java/cn/alphahub/mall/product/web/ItemController.java
@@ -0,0 +1,14 @@
+package cn.alphahub.mall.product.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <b>商品sku Controller</b>
+ *
+ * @author Weasley J
+ * @version 1.0
+ * @date 2021/03/09
+ */
+@Controller
+public class ItemController {
+}
