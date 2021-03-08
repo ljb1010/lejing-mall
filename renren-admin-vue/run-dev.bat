57c2e82eb68d96137132f9e7b9472843ab7484a8
Weasley <1432689025@qq.com>
2021-03-08 23:28:39 +0800

    add modules

--- renren-admin-vue/run-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off & color 0A
-setlocal enabledelayedexpansion
-set name=���springbootӦ��Ϊ��ִ��jar�ļ�
-title %name%                 ����: %date%    ʱ��: %time%
-cls && npm run dev
-pause && exit
